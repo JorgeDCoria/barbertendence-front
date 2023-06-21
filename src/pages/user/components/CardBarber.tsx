f027f42a4e64d2aa19527e1327f49ed35f2e3442
Coria Jorge <coriajorge.dev.apu@gmail.com>
2023-06-21 11:39:34 -0300

    perf/ barberCard, added component only read

--- src/pages/user/components/CardBarber.tsx
+++ src/pages/user/components/CardBarber.tsx
@@ -10,14 +10,20 @@ import {
 } from "@mui/material";
 import perfil from "../../../assets/perfil.jpg";
 interface Props {
+    onlyRead: boolean;
     barber: Barber;
-    selected: boolean;
-    handleClick: (
+    selected?: boolean;
+    handleClick?: (
         e: React.MouseEvent<HTMLDivElement, MouseEvent>,
         barber: Barber
     ) => void;
 }
-const CardBarber: React.FC<Props> = ({ barber, selected, handleClick }) => {
+const CardBarber: React.FC<Props> = ({
+    onlyRead = false,
+    barber,
+    selected,
+    handleClick = () => {},
+}) => {
     const theme: Theme = useTheme();
     const [isHovered, setIsHovered] = useState<boolean>(false);
     const handleMouseEnter = (
@@ -34,83 +40,134 @@ const CardBarber: React.FC<Props> = ({ barber, selected, handleClick }) => {
     };
 
     return (
-        <Card
-            onMouseEnter={(e) => handleMouseEnter(e)}
-            onMouseLeave={(e) => handleMouseLeave(e)}
-            onClick={(e) => handleClick(e, barber)}
-            sx={{
-                height: "280px",
-                width: "230px",
-                position: "relative",
-                cursor: "pointer",
-            }}
-        >
-            <CardMedia
-                component={"img"}
-                src={perfil}
-                sx={{
-                    height: `${isHovered || selected ? "100%" : "200px"}`,
-                    width: "100%",
-                    objectFit: "cover",
-                    borderRadius: "16px",
-                    transition: "height 1s",
-                    scale: `${selected && "1.2"}`,
-
-                    "&:hover": {
-                        scale: `${!selected && "1.2"}`,
-                    },
-                }}
-            />
-            <Typography
-                textAlign={"center"}
-                variant="h4"
-                width={"100%"}
-                color={theme.palette.secondary.main}
-                fontWeight={800}
-                position={"absolute"}
-                top={4}
-                sx={{
-                    WebkitTextStroke: `0.5px white `,
-                }}
-            >
-                {barber.name}
-            </Typography>
-            <Box
-                sx={{
-                    position: "absolute",
-                    bottom: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "40%",
-
-                    display: "flex",
-                    justifyContent: "center",
-                }}
-            >
-                <Box
-                    component={"div"}
-                    p={1}
-                    borderRadius={"16px"}
+        <>
+            {!onlyRead ? (
+                <Card
+                    onMouseEnter={(e) => handleMouseEnter(e)}
+                    onMouseLeave={(e) => handleMouseLeave(e)}
+                    onClick={(e) => handleClick(e, barber)}
                     sx={{
-                        background: `${
-                            isHovered || selected
-                                ? theme.palette.primary.main
-                                : "#282528"
-                        }`,
-
-                        width: "90%",
-                        height: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        transitionDuration: "1.5s",
+                        height: "280px",
+                        width: "230px",
+                        position: "relative",
+                        cursor: "pointer",
                     }}
                 >
-                    <Typography textAlign={"center"} color={"white"}>
-                        {barber.description}{" "}
-                    </Typography>{" "}
-                </Box>
-            </Box>
-        </Card>
+                    <CardMedia
+                        component={"img"}
+                        src={perfil}
+                        sx={{
+                            height: `${
+                                isHovered || selected ? "100%" : "200px"
+                            }`,
+                            width: "100%",
+                            objectFit: "cover",
+                            borderRadius: "16px",
+                            transition: "height 1s",
+                            scale: `${selected && "1.2"}`,
+
+                            "&:hover": {
+                                scale: `${!selected && "1.2"}`,
+                            },
+                        }}
+                    />
+                    <Typography
+                        textAlign={"center"}
+                        variant="h4"
+                        width={"100%"}
+                        color={theme.palette.secondary.main}
+                        fontWeight={800}
+                        position={"absolute"}
+                        top={4}
+                        sx={{
+                            WebkitTextStroke: `0.5px white `,
+                        }}
+                    >
+                        {barber.name}
+                    </Typography>
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            bottom: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "40%",
+
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Box
+                            component={"div"}
+                            p={1}
+                            borderRadius={"16px"}
+                            sx={{
+                                background: `${
+                                    isHovered || selected
+                                        ? theme.palette.primary.main
+                                        : "#282528"
+                                }`,
+
+                                width: "90%",
+                                height: "100%",
+                                display: "flex",
+                                alignItems: "center",
+                                transitionDuration: "1.5s",
+                            }}
+                        >
+                            <Typography textAlign={"center"} color={"white"}>
+                                {barber.description}{" "}
+                            </Typography>{" "}
+                        </Box>
+                    </Box>
+                </Card>
+            ) : (
+                <>
+                    <Card
+                        sx={{
+                            height: "200px",
+                            width: "200px",
+                            position: "relative",
+                            background: "none",
+                            border: "none",
+                            boxShadow: "none",
+                        }}
+                    >
+                        <CardMedia
+                            component={"img"}
+                            src={perfil}
+                            sx={{
+                                height: "100%",
+                                width: "100%",
+                                borderRadius: "50%",
+                            }}
+                        />
+
+                        <Box
+                            sx={{
+                                position: "absolute",
+                                bottom: 0,
+                                left: 0,
+                                width: "100%",
+                                display: "flex",
+                                justifyContent: "center",
+                                background: "#282528",
+                                p: 1,
+                                borderRadius: "16px",
+                            }}
+                        >
+                            <Typography
+                                variant="h6"
+                                textAlign={"center"}
+                                color={"white"}
+                            >
+                                {barber.name}
+                            </Typography>
+                        </Box>
+                    </Card>
+                </>
+            )}
+        </>
     );
 };
 
