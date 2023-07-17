3bb4d775cfc03fe5c1ce8e00554ce6cb2ccb279d
Coria Jorge <coriajorge.dev.apu@gmail.com>
2023-07-17 18:47:53 -0300

    perf/ sideBar setup in mode temporary and persisten

--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -1,33 +1,71 @@
-import { Box, useMediaQuery, Theme, useTheme, Button } from "@mui/material";
+import {
+    Box,
+    useMediaQuery,
+    Theme,
+    useTheme,
+    Button,
+    Stack,
+    Avatar,
+    IconButton,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import SideBar from "./components/SideBar";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const AdminHome: React.FC = () => {
-    const [showSideBar, setShowSideBar] = useState<boolean | undefined>();
     const theme: Theme = useTheme();
-    const isUpSm = useMediaQuery(theme.breakpoints.up("sm"));
+    const isUpMd = useMediaQuery(theme.breakpoints.up("md"));
+    const [showSideBar, setShowSideBar] = useState<boolean>(isUpMd);
+
+    const drawerWidth = 350;
     const handleShowSideBar = () => {
         setShowSideBar((prev) => !prev);
     };
     useEffect(() => {
-        setShowSideBar(isUpSm);
-    }, [isUpSm]);
+        setShowSideBar(isUpMd);
+    }, [isUpMd]);
     return (
-        <Box position={"relative"} minHeight={"100vh"}>
-            <SideBar showSideBar={showSideBar} />
-            Admin
-            {!isUpSm && (
-                <Button
-                    onClick={handleShowSideBar}
+        <Box minHeight={"100vh"}>
+            <Box
+                id="admin-nav"
+                display={"flex"}
+                flexDirection={"row"}
+                border={"2px solid red"}
+                justifyContent={showSideBar ? "flex-end" : "space-between"}
+                sx={{
+                    ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
+                }}
+                px={2}
+                py={2}
+            >
+                <IconButton
                     sx={{
-                        position: "absolute",
-                        bottom: 0,
-                        right: 4,
+                        display: showSideBar ? "none" : "block",
                     }}
+                    onClick={handleShowSideBar}
                 >
-                    {showSideBar ? "Close" : "Open"}
-                </Button>
-            )}
+                    <MenuIcon />
+                </IconButton>
+                <Box display={"flex"} flexDirection={"row"} gap={4}>
+                    <Button variant="outlined">Actualizar</Button>
+                    <Avatar>JC</Avatar>
+                </Box>
+            </Box>
+            <SideBar
+                showSideBar={showSideBar}
+                handleShowSideBar={handleShowSideBar}
+                isUpMd={isUpMd}
+            />
+            <Box
+                id="admin-section"
+                sx={{
+                    background: "gray",
+                    minHeight: "80vh",
+                    ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
+                }}
+            >
+                section isUpMd is {isUpMd}
+            </Box>
         </Box>
     );
 };
--- src/pages/admin/components/SideBar.tsx
+++ src/pages/admin/components/SideBar.tsx
@@ -1,45 +1,141 @@
-import { Box, Theme, useTheme, Card, CardMedia } from "@mui/material";
+import {
+    Box,
+    Theme,
+    useTheme,
+    Card,
+    CardMedia,
+    FormControl,
+    Select,
+    MenuItem,
+    InputLabel,
+    Drawer,
+    IconButton,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 import logo from "../../../assets/logov1.png";
 
 interface Props {
     showSideBar: boolean | undefined;
+    handleShowSideBar: () => void;
+    isUpMd: boolean;
 }
-const SideBar: React.FC<Props> = ({ showSideBar }) => {
+const SideBar: React.FC<Props> = ({ showSideBar, handleShowSideBar, isUpMd }) => {
     const theme: Theme = useTheme();
+    const drawerWidth = 350;
+    const draweHeaderHeight = 48;
     return (
-        <Box
-            width={"350px"}
-            height={"100vh"}
+        <Drawer
+            open={showSideBar}
+            variant={isUpMd ? "persistent" : "temporary"}
             sx={{
-                background: theme.palette.customDark,
-                position: "fixed",
-                top: 0,
-                left: showSideBar ? 0 : "-100%",
-                zIndex: 2,
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
             }}
         >
-            <Card
+            <Box
                 sx={{
-                    width: "200px",
-                    height: "200px",
-                    background: "none",
-                    boxShadow: "none",
+                    background: theme.palette.customDark,
+                    width: drawerWidth,
+                    height: "100%",
                 }}
             >
-                <CardMedia
+                <Box
+                    display={"flex"}
+                    justifyContent={"flex-end"}
+                    alignItems={"center"}
+                    pr={1}
                     sx={{
-                        width: "100%",
-                        height: "100%",
+                        height: `${draweHeaderHeight}px`,
+                        background: theme.palette.customDark,
                     }}
-                    image={logo}
-                />
-            </Card>
-        </Box>
+                >
+                    <IconButton
+                        onClick={handleShowSideBar}
+                        sx={{
+                            color: "white",
+                            "&:hover": {
+                                background: "gray",
+                            },
+                        }}
+                    >
+                        {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </Box>
+                <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    justifyContent={"space-around"}
+                    alignItems={"center"}
+                    border={"2px solid red"}
+                    sx={{ height: `calc(100% - ${draweHeaderHeight}px)` }}
+                >
+                    <Card
+                        sx={{
+                            width: "200px",
+                            height: "200px",
+                            background: "none",
+                            boxShadow: "none",
+                        }}
+                    >
+                        <CardMedia
+                            sx={{
+                                width: "100%",
+                                height: "100%",
+                            }}
+                            image={logo}
+                        />
+                    </Card>
+                    <FormControl fullWidth>
+                        <InputLabel
+                            id="type-appointment"
+                            sx={{
+                                color: "white",
+                            }}
+                        >
+                            {" "}
+                            Turnos Pendientes
+                        </InputLabel>
+                        <Select
+                            labelId="type-appointment"
+                            value={""}
+                            sx={{
+                                border: "1px solid white",
+                            }}
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth>
+                        <InputLabel
+                            id="barber"
+                            sx={{
+                                color: "white",
+                            }}
+                        >
+                            {" "}
+                            all
+                        </InputLabel>
+                        <Select
+                            labelId="barber"
+                            value={""}
+                            sx={{
+                                border: "1px solid white",
+                            }}
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+            </Box>
+        </Drawer>
     );
 };
 
