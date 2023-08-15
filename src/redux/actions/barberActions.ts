b21d091ff638d27d232b5f8056f6f08e78d1857e
Jorge Coria <coriajorge.dev@gmail.com>
2023-08-15 01:05:10 -0300

    perf, state barber, se agrega propiedad en el estado para gestionar barberos a mostrar

--- src/redux/actions/barberActions.ts
+++ src/redux/actions/barberActions.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from "@reduxjs/toolkit";
-import { getAllBarbers } from "../slices/barberSlice";
+import { getAllBarbers, getBarbersSelect } from "../slices/barberSlice";
 import { Barber } from "src/types/Barber";
 import { clearError, setError } from "../slices/errorSlice";
 import { StateError } from "src/types/StateError";
@@ -11,9 +11,16 @@ export const actionGetAllBarber = () => {
             dispatch(clearError());
             const barbers: Barber[] = await barberService.getAllBarbers();
             dispatch(getAllBarbers(barbers));
+            dispatch(getBarbersSelect(barbers));
         } catch (e: any) {
             const error: StateError = { code: e.status, message: e.message };
             dispatch(setError(error));
         }
+    };
+};
+
+export const actionGetSelectBarber = (barbers: Barber[]) => {
+    return (dispatch: Dispatch) => {
+        dispatch(getBarbersSelect(barbers));
     };
 };
--- src/redux/slices/barberSlice.ts
+++ src/redux/slices/barberSlice.ts
@@ -3,10 +3,12 @@ import { Barber } from "src/types/Barber";
 
 interface BarberState {
     barbers: Barber[] | null;
+    barbersSelected: Barber[];
 }
 
 const initialState: BarberState = {
     barbers: null,
+    barbersSelected: [],
 };
 
 export const barberSlice = createSlice({
@@ -16,9 +18,12 @@ export const barberSlice = createSlice({
         getAllBarbers: (state, action: PayloadAction<Barber[]>) => {
             state.barbers = action.payload;
         },
+        getBarbersSelect: (state, action: PayloadAction<Barber[]>) => {
+            state.barbersSelected = action.payload;
+        },
     },
 });
 
-export const { getAllBarbers } = barberSlice.actions;
+export const { getAllBarbers, getBarbersSelect } = barberSlice.actions;
 
 export default barberSlice.reducer;
