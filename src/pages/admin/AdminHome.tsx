cd511c3c1922aba5c2c8ccaea7aea903cb93defa
Jorge Coria <coriajorge.dev@gmail.com>
2023-08-15 01:11:52 -0300

    feat/ hook, se agrega hook personalizado para  inferir tipo de action y stado, se implementa condicion de renderizado en adminhome cuando el estado cuente con datos de appointmens y barbers

--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -4,21 +4,17 @@ import {
     Theme,
     useTheme,
     Button,
-    Stack,
     Avatar,
     IconButton,
     Typography,
 } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import { useAppDispatch, useAppSelector } from "../../hook/useStore";
 import React, { useEffect, useState } from "react";
 import SideBar from "./components/SideBar";
 import MenuIcon from "@mui/icons-material/Menu";
 import AdminSchedule from "../../components/admin-schedule/AdminSchedule";
-import dayjs, { Dayjs } from "dayjs";
-import {
-    actionGetAppointmentsByBarber,
-    actionGetAppointments,
-} from "../../redux/actions/appointmentActions";
+import { actionGetAppointments } from "../../redux/actions/appointmentActions";
+import { actionGetAllBarber } from "../../redux/actions/barberActions";
 import Loading from "../../components/Loading/Loading";
 
 const AdminHome: React.FC = () => {
@@ -27,7 +23,8 @@ const AdminHome: React.FC = () => {
     const [showSideBar, setShowSideBar] = useState<boolean>(isUpMd);
     const [currentDate, setCurrentDate] = useState<Date>(new Date());
 
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
+
     const drawerWidth = 350;
     const handleShowSideBar = () => {
         setShowSideBar((prev) => !prev);
@@ -35,66 +32,72 @@ const AdminHome: React.FC = () => {
     const handleCurrentDate = (date: Date) => {
         setCurrentDate(date);
     };
-    const { appointments } = useSelector((state) => state.appointments);
-    console.log(appointments);
+    const { appointments, barbers } = useAppSelector((state) => state);
 
     useEffect(() => {
         setShowSideBar(isUpMd);
+        dispatch(actionGetAllBarber());
         dispatch(actionGetAppointments());
     }, [isUpMd]);
-    return (
-        <Box minHeight={"100vh"}>
-            <Box
-                id="admin-nav"
-                display={"flex"}
-                flexDirection={"row"}
-                justifyContent={showSideBar ? "flex-end" : "space-between"}
-                sx={{
-                    ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
-                }}
-                px={2}
-                py={2}
-            >
-                <IconButton
+
+    if (!appointments && !barbers) return <>Loading ...</>;
+    else
+        return (
+            <Box minHeight={"100vh"}>
+                <Box
+                    id="admin-nav"
+                    display={"flex"}
+                    flexDirection={"row"}
+                    justifyContent={showSideBar ? "flex-end" : "space-between"}
                     sx={{
-                        display: showSideBar ? "none" : "block",
+                        ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
                     }}
-                    onClick={handleShowSideBar}
+                    px={2}
+                    py={2}
                 >
-                    <MenuIcon />
-                </IconButton>
-                <Box display={"flex"} flexDirection={"row"} gap={4}>
-                    <Button variant="outlined">Actualizar</Button>
-                    <Avatar>JC</Avatar>
+                    <IconButton
+                        sx={{
+                            display: showSideBar ? "none" : "block",
+                        }}
+                        onClick={handleShowSideBar}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box display={"flex"} flexDirection={"row"} gap={4}>
+                        <Button variant="outlined">Actualizar</Button>
+                        <Avatar>JC</Avatar>
+                    </Box>
+                </Box>
+                <SideBar
+                    showSideBar={showSideBar}
+                    handleShowSideBar={handleShowSideBar}
+                    isUpMd={isUpMd}
+                    currentDate={currentDate}
+                    handleChangeDate={handleCurrentDate}
+                />
+                <Box
+                    id="admin-section"
+                    sx={{
+                        minHeight: "80vh",
+                        paddingX: 4,
+                        ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
+                    }}
+                >
+                    <Typography variant="h5" align="center" color={theme.palette.primary.main}>
+                        {" "}
+                        Reservas
+                    </Typography>
+                    {appointments ? (
+                        <AdminSchedule
+                            currentDate={currentDate}
+                            handleChangeDate={handleCurrentDate}
+                        />
+                    ) : (
+                        <Loading />
+                    )}
                 </Box>
             </Box>
-            <SideBar
-                showSideBar={showSideBar}
-                handleShowSideBar={handleShowSideBar}
-                isUpMd={isUpMd}
-                currentDate={currentDate}
-                handleChangeDate={handleCurrentDate}
-            />
-            <Box
-                id="admin-section"
-                sx={{
-                    minHeight: "80vh",
-                    paddingX: 4,
-                    ml: isUpMd && showSideBar ? `${drawerWidth}px` : 0,
-                }}
-            >
-                <Typography variant="h5" align="center" color={theme.palette.primary.main}>
-                    {" "}
-                    Reservas
-                </Typography>
-                {appointments ? (
-                    <AdminSchedule currentDate={currentDate} handleChangeDate={handleCurrentDate} />
-                ) : (
-                    <Loading />
-                )}
-            </Box>
-        </Box>
-    );
+        );
 };
 
 export default AdminHome;
