32749635c01f2e3ce7b2ea709cee66569fdcc0ba
Jorge Coria <coriajorge.dev@gmail.com>
2023-09-04 22:40:08 -0300

    perf/ se modifica estructura de Barber y se crea type Schedule para gestionar horarios de disponibilidad de barbero

--- src/types/Barber.ts
+++ src/types/Barber.ts
@@ -1,16 +1,18 @@
+import { Schedule } from "./Schedule";
 import { Service } from "./Service";
 
 export type Barber = {
-    id: string;
+    id?: string;
     name: string;
     password?: string;
     description: string;
-    observation?: string;
     avatar: string;
     birthDay?: string;
     email?: string;
     phone?: string;
     address?: string;
     services?: Service[];
-    schedules?: [];
+    schedules?: Schedule[];
 };
+//seleccion rapida de horarios (turno mañana, tarde, jornada completa y opcion de personalizar);
+// [{dia: "", incio:"DateTIME", fin:""}]
---
+++ src/types/Schedule.ts
@@ -0,0 +1,5 @@
+export type Schedule = {
+    day: "Lunes" | "Martes" | "Miercoles" | "Jueves" | "Viernes" | "Sabado";
+    start: string;
+    end: string;
+};
