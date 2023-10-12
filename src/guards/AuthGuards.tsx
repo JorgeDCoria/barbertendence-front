e0c7acf18e76e50cacb8b7a6e09bed7f12f128c2
Jorge Coria <coriajorge.dev@gmail.com>
2023-10-11 21:35:23 -0300

    perf/ se hacen modificaciones en la navegacion

--- src/guards/AuthGuards.tsx
+++ src/guards/AuthGuards.tsx
@@ -2,25 +2,40 @@ import { useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { PRIVATEROUTES, PrivateAdminRoutes, PrivateUserRoutes, PublicRoutes, ROL } from "../const";
 import { useAppSelector } from "../hook/useStore";
+import { usePersistData } from "../hook/usePersistData";
 
 interface AuthGuardProps {
     redirectTo?: string;
 }
 export const AuthGuard: React.FC<AuthGuardProps> = () => {
-    const { user } = useAppSelector((state) => state.userSate);
-    const { idBarberShop } = useAppSelector((state) => state.barberShop);
-    const navigate = useNavigate();
-    console.log(idBarberShop);
+    //const { user } = useAppSelector((state) => state.userSate);
+    const { getUser, getIdBarberShop } = usePersistData();
+    //const navigate = useNavigate();
 
     useEffect(() => {
+        console.log("private");
+        // if (getUser() && !user) {
+        //     //llamar api para cargar datos de usuario en la store
+        //     console.log(getUser());
+        // } else {
+        //     navigate(`/${getIdBarberShop()}`, { replace: true });
+        // }
         // if (user) {
         //     if (user.rol == ROL.ADMINKEY)
         //         navigate(`/${PRIVATEROUTES}/${PrivateAdminRoutes.ADMIN}`, { replace: true });
         //     else navigate(`/${PRIVATEROUTES}/${PrivateUserRoutes.USER}`, { replace: true });
         // } else navigate(`/${idBarberShop}/${PublicRoutes.LOGIN}`, { replace: true });
-    }, [idBarberShop]);
+    }, []);
 
-    return null;
+    return false ? (
+        getUser()?.rol === ROL.ADMINKEY ? (
+            <Navigate to={`/${PrivateAdminRoutes.ADMIN}`} replace />
+        ) : (
+            <Navigate to={`/${PrivateUserRoutes.USER}`} replace />
+        )
+    ) : (
+        <Navigate to={`/${getIdBarberShop()}`} replace />
+    );
 };
 
 export default AuthGuard;
